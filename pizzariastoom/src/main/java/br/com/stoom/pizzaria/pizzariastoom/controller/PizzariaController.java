6807b5bb8c3ccd699b238d2937b900615a2b0bc2
Gustavo <gustavo.s.navi@gmail.com>
2020-12-21 05:58:39 -0300

    Criado endpoints

--- pizzariastoom/src/main/java/br/com/stoom/pizzaria/pizzariastoom/controller/PizzariaController.java
+++ pizzariastoom/src/main/java/br/com/stoom/pizzaria/pizzariastoom/controller/PizzariaController.java
@@ -1,103 +1,193 @@
 package br.com.stoom.pizzaria.pizzariastoom.controller;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.google.gson.Gson;
+
 import io.swagger.annotations.ApiOperation;
-import io.swagger.models.Response;
 
 @Controller
-public class PizzariaController<E> {
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RequestMapping(path="request")
+public class PizzariaController<E, T> 
+{
 
-	@SuppressWarnings("unchecked")
-	@ApiOperation(value = "Set type past of pizza")
-	@RequestMapping(value = "/set-pastaType", method = RequestMethod.POST, produces = {MediaType.APPLICATION_JSON_VALUE})
-	public ResponseEntity<List<String>> setPastaType(HttpServletRequest servletRequest, @RequestBody Map<String, String> pastType)
+	@ApiOperation(value = "Get type past of pizza")
+	@RequestMapping(value = "/get-pastaType", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<String> getPastaType(HttpServletRequest req)
 	{
 		
-		List<String> list = new ArrayList<String>();
+		List<String> pastaTypeList = new ArrayList<String>();
 		
-		list.add("Massa PAN");
-		list.add("Massa Fininha");
-		list.add("Massa Artezanale");
-		list.add("Massa Siciliana");
-		list.add("Massa Artezanale com Borda Cheddar");
-		list.add("Massa Artezanale com Borda Requeijão");
+		pastaTypeList.add("Massa PAN");
+		pastaTypeList.add("Massa Fininha");
+		pastaTypeList.add("Massa Artezanale");
+		pastaTypeList.add("Massa Siciliana");
+		pastaTypeList.add("Massa Artezanale com Borda Cheddar");
+		pastaTypeList.add("Massa Artezanale com Borda Requeijão");
+
+		String json = new Gson().toJson(pastaTypeList);
 		
-		return (ResponseEntity<List<String>>) list;
+		return new ResponseEntity<>(json, HttpStatus.OK);
 		
 	}
 	
-	@SuppressWarnings("unchecked")
-	@ApiOperation(value = "Set size of pizza")
-	@RequestMapping(value = "/set-pizzaSize", method = RequestMethod.POST, produces = {MediaType.APPLICATION_JSON_VALUE})
-	public ResponseEntity<List<String>> setSizePizza(HttpServletRequest servletRequest, @RequestBody Map<String, String> pizzaSize)
+
+	@ApiOperation(value = "Get size of pizza")
+	@RequestMapping(value = "/get-pizzaSize", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<String> getSizePizza()
 	{
 		
-		List<String> list = new ArrayList<String>();
+		List<String> pizzaSizeList = new ArrayList<String>();
 		
-		list.add("Pequena (1 Fatia)");
-		list.add("Média (4 Fatias)");
-		list.add("Grande (8 Fatias)");
-		list.add("Família (12 Fatias)");
+		pizzaSizeList.add("Pequena (1 Fatia)");
+		pizzaSizeList.add("Média (4 Fatias)");
+		pizzaSizeList.add("Grande (8 Fatias)");
+		pizzaSizeList.add("Família (12 Fatias)");
 		
-		return (ResponseEntity<List<String>>) list;
+		String json = new Gson().toJson(pizzaSizeList);
+		return new ResponseEntity<>(json, HttpStatus.OK);
 		
 	}
 	
-	@SuppressWarnings("unchecked")
-	@ApiOperation(value = "Set filling of pizza")
-	@RequestMapping(value = "/set-pizzaFilling", method = RequestMethod.POST, produces = {MediaType.APPLICATION_JSON_VALUE})
-	public ResponseEntity<List<String>> setPizzaFilling(HttpServletRequest servletRequest, @RequestBody Map<String, String> pizzaFilling)
+
+	@ApiOperation(value = "Get filling of pizza")
+	@RequestMapping(value = "/get-pizzaFilling", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<String> getPizzaFilling()
 	{
 		
-		List<String> list = new ArrayList<String>();
+		List<String> pizzaFillingList = new ArrayList<String>();
 		
-		list.add("Calabresa com Cebola");
-		list.add("Calabresa sem Cebola");
-		list.add("Portuguesa");
-		list.add("Frango com Catupiry");
-		list.add("Muçarela");
-		list.add("Napolitana");
-		list.add("Brigadeiro");
-		list.add("Romeu e Julieta");
-		list.add("Creme de Avelã com Frutas");
-		list.add("Califórnia");
+		pizzaFillingList.add("Calabresa com Cebola");
+		pizzaFillingList.add("Calabresa sem Cebola");
+		pizzaFillingList.add("Portuguesa");
+		pizzaFillingList.add("Frango com Catupiry");
+		pizzaFillingList.add("Muçarela");
+		pizzaFillingList.add("Napolitana");
+		pizzaFillingList.add("Brigadeiro");
+		pizzaFillingList.add("Romeu e Julieta");
+		pizzaFillingList.add("Creme de Avelã com Frutas");
+		pizzaFillingList.add("Califórnia");
 		
-		return (ResponseEntity<List<String>>) list;
+		String json = new Gson().toJson(pizzaFillingList);
+		return new ResponseEntity<>(json, HttpStatus.OK);
 		
 	}
 	
-	@SuppressWarnings("unchecked")
-	@ApiOperation(value = "Set side dish of pizza")
-	@RequestMapping(value = "/set-pizzaSideDish", method = RequestMethod.POST, produces = {MediaType.APPLICATION_JSON_VALUE})
-	public ResponseEntity<List<String>> setPizzaSideDish(HttpServletRequest servletRequest, @RequestBody Map<String, String> pizzaSideDish)
+	@ApiOperation(value = "Get side dish of pizza")
+	@RequestMapping(value = "/get-pizzaSideDish", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<String> getPizzaSideDish()
 	{
 		
-		List<String> list = new ArrayList<String>();
+		List<String> pizzaSideDishList = new ArrayList<String>();
 		
-		list.add("Coca Cola 2L");
-		list.add("Coca Cola 600ml");
-		list.add("Guaraná 2L");
-		list.add("Guaraná 600ml");
-		list.add("Bata Frita (200g)");
-		list.add("Cerveja Original 600ml");
-		list.add("Cerveja Original 350ml");
-		list.add("Espetinho de Carne");
-		list.add("Patê de Frango");
-		list.add("Salada de Alface");
+		pizzaSideDishList.add("Coca Cola 2L");
+		pizzaSideDishList.add("Coca Cola 600ml");
+		pizzaSideDishList.add("Guaraná 2L");
+		pizzaSideDishList.add("Guaraná 600ml");
+		pizzaSideDishList.add("Bata Frita (200g)");
+		pizzaSideDishList.add("Cerveja Original 600ml");
+		pizzaSideDishList.add("Cerveja Original 350ml");
+		pizzaSideDishList.add("Espetinho de Carne");
+		pizzaSideDishList.add("Patê de Frango");
+		pizzaSideDishList.add("Salada de Alface");
 		
-		return (ResponseEntity<List<String>>) list;
+		String json = new Gson().toJson(pizzaSideDishList);
+		return new ResponseEntity<>(json, HttpStatus.OK);
 		
 	}
+	
+	@ApiOperation(value = "Get the Pizza of the Day")
+	@RequestMapping(value = "/get-pizzaOfDay", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+	public ResponseEntity<String> getPizzaOfDay()
+	{
+		boolean hasError = false;
+		List<String> pizzaOfDay = new ArrayList<String>();
+		
+		int dayOfWeek = LocalDate.now().getDayOfWeek().getValue();
+		
+		switch(dayOfWeek)
+		{
+			//Monday
+			case 1:
+				pizzaOfDay.add("Tipo da Massa: Massa PAN");
+				pizzaOfDay.add("Tamanho da Pizza: Grande (8 Fatias)");
+				pizzaOfDay.add("Sabor da Pizza: Calabresa com Cebola");
+				pizzaOfDay.add("Pontos ao adquirir esta pizza: 20");
+				break;
+			
+			//Tuesday
+			case 2:
+				pizzaOfDay.add("Tipo da Massa: Massa Fininha");
+				pizzaOfDay.add("Tamanho da Pizza: Grande (8 Fatias)");
+				pizzaOfDay.add("Sabor da Pizza: Portuguesa");
+				pizzaOfDay.add("Pontos ao adquirir esta pizza: 20");
+				break;
+			
+			//Wednesday
+			case 3:
+				pizzaOfDay.add("Tipo da Massa: Massa Artezanale");
+				pizzaOfDay.add("Tamanho da Pizza: Grande (8 Fatias)");
+				pizzaOfDay.add("Sabor da Pizza: Muçarela");
+				pizzaOfDay.add("Pontos ao adquirir esta pizza: 10");
+				break;
+			
+			//Thursday	
+			case 4:
+				pizzaOfDay.add("Tipo da Massa: Massa Siciliana");
+				pizzaOfDay.add("Tamanho da Pizza: Grande (8 Fatias)");
+				pizzaOfDay.add("Sabor da Pizza: Napolitana");
+				pizzaOfDay.add("Pontos ao adquirir esta pizza: 10");
+				break;
+				
+			//Friday
+			case 5:
+				pizzaOfDay.add("Tipo da Massa: Massa Artezanale");
+				pizzaOfDay.add("Tamanho da Pizza: Grande (8 Fatias)");
+				pizzaOfDay.add("Sabor da Pizza: Frango com Catupiry");
+				pizzaOfDay.add("Pontos ao adquirir esta pizza: 10");
+				break;
+				
+			//Saturday
+			case 6:
+				pizzaOfDay.add("Tipo da Massa: Massa Artezanale com Borda Cheddar");
+				pizzaOfDay.add("Tamanho da Pizza: Família (12 Fatias)");
+				pizzaOfDay.add("Sabor da Pizza: Califórnia");
+				pizzaOfDay.add("Pontos ao adquirir esta pizza: 15");
+				break;
+				
+			//Sunday
+			case 7:
+				pizzaOfDay.add("Tipo da Massa: Massa Artezanale com Borda Requeijão");
+				pizzaOfDay.add("Tamanho da Pizza: Família (12 Fatias)");
+				pizzaOfDay.add("Sabor da Pizza: Romeu e Julieta");
+				pizzaOfDay.add("Pontos ao adquirir esta pizza: 15");
+				break;
+			
+			default:
+				hasError = true;
+				
+		}
+		
+		if(hasError)
+		{
+			return new ResponseEntity<>("Internal Server Error!", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		String json = new Gson().toJson(pizzaOfDay);
+		return new ResponseEntity<>(json, HttpStatus.OK);
+	}
+	
 }
